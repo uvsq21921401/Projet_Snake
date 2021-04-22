0d1296457f6c72180c0223e827c01d29528846a7
Pauline STAELENS <pauline.staelens@ens.uvsq.fr>
2021-04-22 15:10:33 +0200

    Base du canvas

--- Snake_FilinaDERIAN_SarahGUERFA_LiciaBENKHEROUF_PaulineSTAELENS.py
+++ Snake_FilinaDERIAN_SarahGUERFA_LiciaBENKHEROUF_PaulineSTAELENS.py
@@ -1,2 +1,47 @@
-#SARAH GUERFA#
-chuvhjfkv
+#######################################
+# groupe Bi TD-04
+# BENKHEROUF Licia
+# DERIAN Filina
+# GUERFA Sarah
+# STAELENS Pauline
+# https://github.com/uvsq21921401/Projet_Snake
+#######################################
+
+#######################################
+# Import des librairies
+
+import tkinter as tk
+
+#######################################
+# Constantes (écrites en majuscule)
+
+LARGEUR = 500
+HAUTEUR = 500
+COTE = 20
+COLONE = LARGEUR // COTE
+LIGNE = HAUTEUR // COTE
+
+#######################################
+# Variables globales
+
+#######################################
+# Fonctions
+
+#######################################
+# Programme principale
+
+racine = tk.Tk()
+racine.title("Snake")
+
+# Création des widgets
+
+canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR, bg="green")
+
+# Placement des widgets
+
+canvas.grid()
+
+# Événements
+
+
+racine.mainloop()
